faf7d800b45fc3bb93b5cccd857ed2724dfddf9d
Lane <lolson@ualberta.ca>
2012-11-26 10:32:07 -0700

    institutional script

---
+++ js/ualberta/institutional.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+		var winHeight = $(window).height();
+		var headHeight = $('header').height();
+		var footerHeight = $('footer').height();
+		var bodyMinHeight = winHeight-(headHeight+footerHeight+20);
+		$('.content-wrapper').css('min-height', bodyMinHeight+'px');
+}); 
