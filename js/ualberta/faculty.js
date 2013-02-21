1f3c4a4d7d34267c7d8198594006293f01282feb
Kyle Schmidt <kyle.schmidt@ualberta.ca>
2013-02-21 10:14:07 -0700

    Added name changing to navigation titles.

--- js/ualberta/faculty.js
+++ js/ualberta/faculty.js
@@ -1,10 +1,26 @@
 $(document).ready(function() {
-		var winHeight = $(window).height();
-		var headHeight = $('header').height();
-		var footerHeight = $('footer').height();
-		var bodyMinHeight = winHeight-(headHeight+footerHeight+20);
-		$('.content-wrapper').css('min-height', bodyMinHeight+'px');
-		$('.heading-link').tooltip();
-		
-		$('body').on('touchstart.dropdown', '.dropdown-menu', function (e) { e.stopPropagation(); });
+	var winHeight = $(window).height();
+	var headHeight = $('header').height();
+	var footerHeight = $('footer').height();
+	var bodyMinHeight = winHeight-(headHeight+footerHeight+20);
+	$('.content-wrapper').css('min-height', bodyMinHeight+'px');
+	$('.heading-link').tooltip();
+
+	var navTitle = $('#main-nav .navigation-toggle span');
+	$('#main-nav .main-navigation').on('show', function() {
+		navTitle.html('Hide Navigation');
+	});
+	$('#main-nav .main-navigation').on('hide', function() {
+		navTitle.html('Show Navigation');
+	});
+
+	var sectionNavTitle = $('.section-nav .navigation-toggle span');
+	$('.leftnav').on('show', function() {
+		sectionNavTitle.html('Hide Section Navigation');
+	});
+	$('.leftnav').on('hide', function() {
+		sectionNavTitle.html('Show Section Navigation');
+	});
+
+	$('body').on('touchstart.dropdown', '.dropdown-menu', function (e) { e.stopPropagation(); });
 }); 
--- js/ualberta/institutional.js
+++ js/ualberta/institutional.js
@@ -1,13 +1,21 @@
 $(document).ready(function() {
-		var winHeight = $(window).height();
-		var headHeight = $('header').height();
-		var footerHeight = $('footer').height();
-		var bodyMinHeight = winHeight-(headHeight+footerHeight+20);
-		$('.content-wrapper').css('min-height', bodyMinHeight+'px');
-		$('.content-container.container').css('min-height', $('.sidebar').height()+'px');
-		$('.heading-link').tooltip();
+	var winHeight = $(window).height();
+	var headHeight = $('header').height();
+	var footerHeight = $('footer').height();
+	var bodyMinHeight = winHeight-(headHeight+footerHeight+20);
+	$('.content-wrapper').css('min-height', bodyMinHeight+'px');
+	$('.content-container.container').css('min-height', $('.sidebar').height()+'px');
+	$('.heading-link').tooltip();
+
+	var navTitle = $('#main-nav .navigation-toggle span');
+	$('#main-nav .main-navigation').on('show', function() {
+		navTitle.html('Hide Navigation');
+	});
+	$('#main-nav .main-navigation').on('hide', function() {
+		navTitle.html('Show Navigation');
+	});
   
-    $('.start').removeClass('start');
-    
-    $('body').on('click touchcancel.dropdown.data-api touchleave.dropdown.data-api touchmove.dropdown.data-api touchend.dropdown.data-api touchstart.dropdown.data-api', '.quick-links', function (e) { e.stopPropagation(); });
+	$('.start').removeClass('start');
+
+	$('body').on('click touchcancel.dropdown.data-api touchleave.dropdown.data-api touchmove.dropdown.data-api touchend.dropdown.data-api touchstart.dropdown.data-api', '.quick-links', function (e) { e.stopPropagation(); });
 }); 
